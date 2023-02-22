6b2fe11c1d9edf7f18a6d5cc0546ba2f9232f615
Michał Gomoła <michal.gomola.89@gmail.com>
2023-02-22 22:16:26 +0100

    some code refactor

--- AdditionalTasks/src/main/java/com/info/javabasics/App.java
+++ AdditionalTasks/src/main/java/com/info/javabasics/App.java
@@ -7,31 +7,44 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.Set;
 
 public class App
 {
     public static void main( String[] args ) {
+
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Give me some string to count letters in it: ");
-        String startingString = scan.nextLine();
-        startingString = startingString.replaceAll(" ","");
+        String strToCount = scan.nextLine();
+        strToCount = strToCount.toLowerCase().replaceAll(" ","");
 
         List<String> lettersToCount = new ArrayList<>();
-        Collections.addAll(lettersToCount, startingString.toLowerCase().split(""));
+        Collections.addAll(lettersToCount, strToCount.split(""));
+
         Set<String> keySet = new HashSet<>(lettersToCount);
-        System.out.println(keySet);
+        printSortedMap(keySet, lettersToCount);
+
+        scan.close();
+    }
+
+    public static void printSortedMap(Set<String> letterSet, List<String> lettersToCount) {
 
         Map<String, Long> mapToSort = new HashMap<>();
         long keyValue;
 
-        for (String s : keySet) {
-            keyValue = lettersToCount.stream().filter(ch -> ch.equals(s)).count();
+        for (String s : letterSet) {
+            keyValue = lettersToCount.stream()
+                    .filter(ch -> ch.equals(s))
+                    .count();
             mapToSort.put(s, keyValue);
         }
-        List<Map.Entry<String, Long>> sortedList = new ArrayList<>(mapToSort.entrySet());
-        sortedList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        List<Entry<String, Long>> sortedList = new ArrayList<>(mapToSort.entrySet());
+        sortedList.sort(Entry.comparingByValue(Comparator.reverseOrder()));
         sortedList.forEach(System.out::println);
     }
+
+
 }
