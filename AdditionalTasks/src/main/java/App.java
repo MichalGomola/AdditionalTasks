2107cda3e4a9792702ea047c25d5babcc552bd50
Michał Gomoła <michal.gomola.89@gmail.com>
2023-03-12 15:01:16 +0100

    updated after review

--- AdditionalTasks/src/main/java/App.java
+++ AdditionalTasks/src/main/java/App.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 public class App {
     public static void main(String[] args) {
         Stream<Integer> data = Stream.iterate(1, i -> i + 1).limit(100);
-        List<Integer> result = new Task1<Integer>().paging(10,1,data);
+        List<Integer> result = Task1.paging(10,3,data);
         System.out.println(result);
     }
 }
